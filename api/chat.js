03a86d99dca4726a6a88d5088ff4b13bfc0a9078
stylix04-max <stylix04@yahoo.co.uk>
2026-06-30 18:33:16 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,3 +1,7 @@
+export const config = {
+  maxDuration: 60,
+};
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
